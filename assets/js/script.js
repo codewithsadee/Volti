229c30d52fee344b8d412654016dbb4cbfb20c2e
Sadee <mohammadsadee24@gmail.com>
2022-10-26 06:49:14 +0600

    intial

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+
+/**
+ * #PRELOADING
+ */
+
+const loadElement = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  loadElement.classList.add("loaded");
+});
+
+
+
+/**
+ * #MOBILE NAVBAR TOGGLE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", toggleNavbar);
+
+
+
+/**
+ *  #HEADER
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-go-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * #SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0, x = revealElements.length; i < x; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
---
+++ style-guide.md
@@ -0,0 +1,82 @@
+# Essential Stuff
+
+## Html import links
+
+Ionicon
+
+``` html
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+```
+
+---
+
+## Colors
+
+``` css
+--rich-black-fogra-29: hsl(230, 14%, 9%);
+--gray-x-11-gray: hsl(0, 0%, 73%);
+--oxford-blue_a15: hsla(216, 65%, 10%, 0.15);
+--yellow-green: hsl(86, 54%, 51%);
+--smoky-black: hsl(0, 0%, 6%);
+--black-coral: hsl(231, 6%, 40%);
+--light-gray: hsl(0, 0%, 80%);
+--cultured: hsl(240, 10%, 94%);
+--white: hsl(0, 0%, 100%);
+--white_a30: hsla(0, 0%, 100%, 0.3);
+--black: hsl(0, 0%, 0%);
+```
+
+## Typography
+
+``` css
+--ff-quicksand: "Quicksand", sans-serif;
+--ff-nunito_sans: "Nunito Sans", sans-serif;
+
+--fs-1: 5.5rem;
+--fs-2: 3.6rem;
+--fs-3: 3.2rem;
+--fs-4: 2.6rem;
+--fs-5: 2.4rem;
+--fs-6: 2.2rem;
+--fs-7: 2rem;
+--fs-8: 1.8rem;
+--fs-9: 1.5rem;
+--fs-10: 1.4rem;
+--fs-11: 1.3rem;
+
+--fw-500: 500;
+--fw-600: 600;
+--fw-700: 700;
+```
+
+## Spacing
+
+``` css
+--section-padding: 40px;
+```
+
+## Shadow
+
+``` css
+--shadow: 0 15px 40px 0 hsla(224, 24%, 47%, 0.1);
+```
+
+## Border Radius
+
+``` css
+--radius-circle: 50%;
+--radius-60: 60px;
+--radius-50: 50px;
+--radius-30: 30px;
+--radius-20: 20px;
+--radius-5: 5px;
+```
+
+## Transition
+
+``` css
+--transition-1: 0.25s ease;
+--transition-2: 0.5s ease;
+--cubic-out: cubic-bezier(0.33, 0.85, 0.4, 0.96);
+```
